a5939b25a7cb37adbde488044c30ec59bbb4aafa
Konstantin Skrypak <konstantin.s@2muchcoffee.com>
2019-03-13 17:34:30 +0200

    delete card

--- src/shared/components/Card/index.tsx
+++ src/shared/components/Card/index.tsx
@@ -1,9 +1,7 @@
 import React, { ReactNode } from 'react';
-import { CardItem, Card } from 'native-base';
+import { CardItem } from 'native-base';
 import ForwardButton from '../ForwardButton';
-import PATHS from '../../paths';
 import { NavigationScreenProp } from 'react-navigation';
-import styles from './style';
 
 type Props<T> = {
   data: T;
@@ -13,12 +11,10 @@ type Props<T> = {
 };
 function CardData<T>({ data, children, navigation, routeName }: Props<T>) {
   return (
-    <Card style={styles.card}>
-      <CardItem>
-        {children}
-        <ForwardButton<T> navigation={navigation} data={data} routeName={routeName} />
-      </CardItem>
-    </Card>
+    <CardItem>
+      {children}
+      <ForwardButton<T> navigation={navigation} data={data} routeName={routeName} />
+    </CardItem>
   );
 }
 export default CardData;
